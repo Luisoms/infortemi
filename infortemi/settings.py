be3b8578ef5dd79ab5e3c07ab7e07e2756fbfe5b
luisoms <lomsloms13@gmail.com>
2022-09-30 14:42:27 -0400

    version 0.0.2

--- infortemi/settings.py
+++ infortemi/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'infortemi.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = 'sqlite:///' + BASE_DIR / 'db.sqlite3',
+        default = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age = 600
     )
 }
